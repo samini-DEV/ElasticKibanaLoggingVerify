96fb737419ecaaed09cda0ce2e3675ce41376e47
parmooz <m.parmooz@ryanmail.ir>
2021-04-06 10:36:44 +0430

    Add project files.

---
+++ ElasticKibanaLoggingVerify/Controllers/ElasticSearchController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticKibanaLoggingVerify.Controllers
+{
+    
+    public class ElasticSearchController : Controller
+    {
+        private readonly ILogger<ElasticSearchController> _logger;
+
+        public ElasticSearchController(ILogger<ElasticSearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/values  
+        [HttpGet]
+        public int GetRandomvalue()
+        {
+            var random = new Random();
+            var randomValue = random.Next(0, 100);
+            _logger.LogInformation($"Random Value is {randomValue}");
+            return randomValue;
+        }
+    }
+}
---
+++ ElasticKibanaLoggingVerify/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ElasticKibanaLoggingVerify
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+                {
+                    ModifyConnectionSettings=x=>x.BasicAuthentication("elastic","Mohsen50")  ,
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
+                })
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureAppConfiguration(configuration =>
+                {
+                    configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    configuration.AddJsonFile(
+                        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
+                })
+        .UseSerilog();
+    }
+}
